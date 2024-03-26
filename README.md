fa5d86708405f3cbc780ca10693c04fbe36b6580
sarahelshark <sarahelsh183@gmail.com>
2024-03-26 13:23:37 +0100

    Add methods

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Concetti di classe, variabili e metodi d'istanza che abbiamo visto, create un file index.php in cui:
 1) è definita una classe ‘Movie’
   => all'interno della classe sono dichiarate delle variabili d'istanza (ok)
-  => all'interno della classe è definito un costruttore
-  => all'interno della classe è definito almeno un metodo
+  => all'interno della classe è definito un costruttore (ok)
+  => all'interno della classe è definito almeno un metodo(ok setCast)
 2) vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
 3) Modificare la classe Movie in modo che accetti piú di un genere.
 4) Creare un layout completo per stampare a schermo una lista di movies.
--- index.php
+++ index.php
@@ -15,40 +15,86 @@ class Movie
      //select an object property inside a class using the $this variable
      $this -> title = $title;
      $this -> overview = $overview;
-     $this
-    
+     $this -> cast = $cast;
+     $this -> duration = $duration;
+     
+    }
+
+    public function setCast($cast)
+    {
+        $this->cast = $cast;
+    }
+    public function getCast()
+    {
+        //in quanto getter, prende info da oggetto richiamato, non richiede param
+        return $this -> cast;
     }
 }
+
+//DIFF METODO E FUNZIONE ?
+//il metodo è una funzione definita dentro una classe, che può essere richiamata da un oggetto 
 
 // Create an instance of a class (an object) with the `new` keyword 1
 $padrino = new Movie('Il Padrino', 'Il padrino lorem ipsum dolo',  [ ] , 120 ) ;
 
+$padrino -> setCast([
+    "Marlon Brando",
+    "Al Pacino",
+    "James Caan",
+    "Robert Duval",
+    "Diane Keaton",
+    "Richard S.Castellano",
+    "Abe Vigoda",
+    "Sterling Hayden",
+    "John Marley",
+    "Richard Conte",
+    "Al Lettieri",
+    "Gianni Russo",
+    "Talia Shire",
+]);
 //write values to each propeerty of the object 2 > 
 //con il __contruct , li passo qui sopra new Movie(dentro qui )
 //$padrino->title = 'il padrino';
 //$padrino->overview = 'lorem ipsumo dolor';
 //$padrino->duration = '120';
-
-var_dump($padrino);
-
-
+//var_dump($padrino);
 //create a second instance
-$matrix = new Movie;
+//writes values to each property of the object (or if they exist already, updates the existing values)
+$matrix = new Movie('Matrix', 'Matrix lor lorem ipsum dolo',  [ 
+    "Keanu Reeves",
+    "Laurence Fishburne",
+    "Carrie-Anne Moss",
+    "Hugo Weaving",
+    "Joe Pantoliano",
+    "Gloria Foster",
+    "Marcus Chong",
+    "Julian Arahanga",
+    "Matt Doran",
+    "Belinda McClory",
+    "Anthony Ray Parker",
+    "Paul Goddard",
 
-$matrix->title = 'The Matrix';
-$matrix->overview = 'lorem ipsumfvfo dolor';
-$matrix->duration = '60';
+] , 60 ) ;
 
-var_dump($matrix);
+//$matrix->title = 'The Matrix';
+//$matrix->overview = 'lorem ipsumfvfo dolor';
+//$matrix->duration = '60';
+
+//var_dump($matrix);
 
 //create a third instance
-$avatar = new Movie;
+$avatar = new Movie('Avatar', 'Avatar lor lorem ipsum dolo',  [
+    "Lorem ipsumo",
+    "dolor sit",
+    "amet et",
+    "volummptat est",
+    "Pinco Pallino",
+ ] , 180 ) ;
 
-$avatar->title = 'Avatar';
-$avatar->overview = 'lorem ipsumfvfo dolor';
-$avatar->duration = '250';
-
-var_dump($avatar);
+//$avatar->title = 'Avatar';
+//$avatar->overview = 'lorem ipsumfvfo dolor';
+//$avatar->duration = '250';
+//var_dump($avatar);
 
 
 //construct= "magic method" !!  faccio tutte le assegnazioni dentro la classe con funzione:
